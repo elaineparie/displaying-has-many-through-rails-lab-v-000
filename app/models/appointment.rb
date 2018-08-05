15b106e9e11abff1c50bd5f61635102b7d6dfed3
Elaine Anderson <eparie@bu.edu>
2018-08-05 23:33:14 +0000

    automatically backed up by learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,8 +2,4 @@ class Appointment < ActiveRecord::Base
   belongs_to :patient
   belongs_to :doctor
 
-  def formatted_date
-    date = DateTime.new(self.appointment_datetime)
-    date.strftime("%B")
-  end
 end
